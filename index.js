932fe8607f5e090291bcf21f390c25ba0c7136e9
Abdelhadi Omar <abdelhadioumar@gmail.com>
2023-05-04 11:35:32 +0100

    markup and styling

---
+++ index.js
@@ -0,0 +1,12 @@
+// main variables
+let theInput = document.querySelector('.get-repos input');
+let getButton = document.querySelector('.get-button');
+let reposData = document.querySelector('.show-data');
+
+//get repos function
+
+function getRepos() {
+    console.log('Function get repos');
+}
+
+getRepos();
